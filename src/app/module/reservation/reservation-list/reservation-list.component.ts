86463ba4ebc41195275add57b5f9692d9431225c
heshanu <heshanu97@gmail.com>
2024-02-15 00:41:07 +0530

    implemted

--- src/app/module/reservation/reservation-list/reservation-list.component.ts
+++ src/app/module/reservation/reservation-list/reservation-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../../../service/reservation/reservation.service';
+import { Reservation } from '../../../model/reservation';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.css'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit{
+
+  constructor(private reservationService:ReservationService){}
+
+  ngOnInit(): void {
+    //this.reservationList=this.reservationService.
+  }
+
+  reservationList:Reservation[]=[];
+
 
 }
